d60574d4f689dad58c01e9b5aa58fa4b464537bd
Ky Van Doan <kyvandoan@users.noreply.github.com>
2025-12-02 14:40:27 +0700

    Correcting wrong version for Day 2

--- day-2/day-2.php
+++ day-2/day-2.php
@@ -1,14 +1,15 @@
 <?php
 $puzzleInput = file('input.txt');
-$productIdsRange = explode(',', $puzzleInput);
-$regexRepeatingSequence = '\b(\d+)\1\b';
-$invalidIds = array();
+$productIdsRange = explode(',', $puzzleInput[0]);
+$invalidIds = 0;
 
 foreach ($productIdsRange as $range) {
     $idRange = explode('-', $range);
     foreach (range($idRange[0], $idRange[1]) as $productId) {
-        preg_match($regexRepeatingSequence, $productId, $invalidIds);
+        if (preg_match('/\b(\d+)\1\b/', $productId)) {
+            $invalidIds = $invalidIds + $productId;
+        }
     }
 }
 
-print "All Invalid IDs Added Up:" . array_sum($invalidIds);
+print "All Invalid IDs Added Up: " . $invalidIds;
