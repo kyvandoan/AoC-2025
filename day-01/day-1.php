a448b3c639bb406f18f8cdb6f676579428ea7e6b
Ky Van Doan <github@mel.kyvan.fr>
2025-12-09 12:04:43 +0700

    Adding Script for Day 1 Part 2

--- day-01/day-1.php
+++ day-01/day-1.php
@@ -3,6 +3,7 @@ $puzzleInput = file('input.txt');
 $dialClicks = 100;
 $dialPosition = 50;
 $password = 0;
+$passwordMethod = 0;
 
 function dialLeft($dialPosition, $rotationDistance, $dialClicks) {
     $dialPosition = $dialPosition - $rotationDistance;
@@ -22,9 +23,34 @@ function dialRight($dialPosition, $rotationDistance, $dialClicks) {
     return $dialPosition;
 }
 
+function countZeroCrossings($startPosition, $direction, $rotationDistance, $dialClicks) {
+    $crossings = 0;
+    
+    if ($startPosition == 0) {
+        return floor($rotationDistance / $dialClicks);
+    }
+
+    if ($direction == 'L') {
+        if ($rotationDistance >= $startPosition) {
+            $remainingDistance = $rotationDistance - $startPosition;
+            $crossings = 1 + floor($remainingDistance / $dialClicks);
+        }
+    } else {
+        $distanceToZero = $dialClicks - $startPosition;
+        if ($rotationDistance >= $distanceToZero) {
+            $remainingDistance = $rotationDistance - $distanceToZero;
+            $crossings = 1 + floor($remainingDistance / $dialClicks);
+        }
+    }
+    
+    return $crossings;
+}
+
 foreach ($puzzleInput as $rotation) {
     $rotationHeading = substr($rotation, 0, 1);
-    $rotationDistance = substr($rotation, 1);
+    $rotationDistance = intval(substr($rotation, 1));
+
+    $previousPosition = $dialPosition;
 
     if ($rotationHeading == 'L') {
         $dialPosition = dialLeft($dialPosition, $rotationDistance, $dialClicks);
@@ -35,7 +61,9 @@ foreach ($puzzleInput as $rotation) {
     if ($dialPosition == 0) {
         $password++;
     }
+
+    $passwordMethod += countZeroCrossings($previousPosition, $rotationHeading, $rotationDistance, $dialClicks);
 }
 
-return "Password: " . $password;
-?>
+print "[Part 1] Password: " . $password . PHP_EOL;
+print "[Part 2] 0x434C49434B Password: " . $passwordMethod;
